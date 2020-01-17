0867eac0aac4a13330d3f590205af035d98a1ec0
André Schlinke <schlinke.andre@gmail.com>
2020-01-17 19:55:32 +0100

    First commit

---
+++ src/Template/Template.h
@@ -0,0 +1,15 @@
+/*
+ * Template.h
+ *
+ *  Created on: Sep 14, 2019
+ *      Author: André Schlinke
+ */
+
+#ifndef SRC_TEMPLATE_TEMPLATE_H_
+#define SRC_TEMPLATE_TEMPLATE_H_
+
+int Mytemplate();
+
+
+
+#endif /* SRC_TEMPLATE_TEMPLATE_H_ */
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Sep 14, 2019
+ *      Author: André Schlinke
+ */
+
+#include <iostream>
+#include "Template/Template.h"
+
+int main()
+{
+  std::cout << "Hello World!\n";
+}
+
