dc354ee490ce94fe3648ac33e172bf9ff9818c18
André Schlinke <schlinke.andre@gmail.com>
2020-03-25 22:24:08 +0100

    Verbose Ctest

--- test/test.cpp
+++ test/test.cpp
@@ -18,11 +18,6 @@
  */
 #include "test.hpp"
 
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
-
 TEST(example, sum_zero) {
   auto integers = {1, -1, 2, -2, 3, -3};
   auto result = sum_integers(integers);
