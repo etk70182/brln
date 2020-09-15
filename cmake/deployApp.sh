4dab2270b6bb950beb97b4b8a94255c2b3ecf150
André Schlinke <schlinke.andre@gmail.com>
2020-09-16 01:15:26 +0200

    One last try

--- cmake/deployApp.sh
+++ cmake/deployApp.sh
@@ -4,6 +4,8 @@ cp -r $1/bin brln.AppDir/usr/bin
 cp $1/_deps/libpd-src/libs/libpd.so brln.AppDir/usr/lib/.
 cp $1/_deps/libpd-src/libs/libpd.so brln.AppDir/usr/lib/.
 cp -r `dirname "$SCRIPT_PATH"`/share brln.AppDir/usr/share
+cp `dirname "$SCRIPT_PATH"`/share/applications/brln.desktop brln.AppDir/.
+cp `dirname "$SCRIPT_PATH"`/share/icons/hicolor/400x400/brln.png brln.AppDir/.
 
 # Get and run linuxdeployqt
 wget -c https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage
