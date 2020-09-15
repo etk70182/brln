c0e6fb908b32b8918bde7d87183d65bb4c73f548
André Schlinke <schlinke.andre@gmail.com>
2020-09-16 00:31:28 +0200

    Add deployApp.sh

---
+++ cmake/deployApp.sh
@@ -0,0 +1,13 @@
+SCRIPT_PATH="${BASH_SOURCE[0]}"
+mkdir -p brln.AppDir/usr/lib
+cp -r $1/bin brln.AppDir/usr/bin
+cp $1/_deps/libpd-src/libs/libpd.so brln.AppDir/usr/lib/.
+cp $1/_deps/libpd-src/libs/libpd.so brln.AppDir/usr/lib/.
+cp -r `dirname "$SCRIPT_PATH"`/share brln.AppDir/usr/share
+
+# Get and run linuxdeployqt
+wget -c https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage
+chmod a+x linuxdeployqt-continuous-x86_64.AppImage
+
+# Create AppImage using linuxdeployqt
+./linuxdeployqt-continuous-x86_64.AppImage ./brln.AppDir/usr/bin/brln -appimage
